52aa94f65407d09920f64e0c235f7aa7046c4865
Gangtaemin <62370073+Gangtaemin@users.noreply.github.com>
2020-09-24 16:13:02 +0900

    Add files via upload

--- aa1-02-rpt03/app.js
+++ aa1-02-rpt03/app.js
@@ -1,20 +1,29 @@
 // app.js
-var express = require('express')
+var express = require("express");
 var app = express();
-var port = 3000;
+var 포트 = 3000;
 
 app.get("/", function (req, res) {
-    res.send('<a href="http://www.naver.com">Go to naver</a>');
+  res.send('<a href="/hello"> Hello Page </a>');
 });
 
-app.get("/hello", function (req, res) {
-    res.send("Hello aa02, Comsi! My first express server!!!");
+app.get("/ hello", function (req, res) {
+  res.send("Hello aa02!");
 });
 
-app.get("/comsi", function (req, res) {
-    res.send("Hello Comsi!");
+app.get("/ comsi", function (req, res) {
+  res.send("Hello Comsi!");
 });
 
+app.get("/ aa03", function (req, res) {
+  res.send("안녕하세요 aa03, Comsi! 내 첫 익스프레스 서버 !!!");
+});
+
+app.get("/ naver", function (req, res) {
+  res.send('<a href="http://www.naver.com"> naver로 이동 </a>');
+});
+
+// 클라이언트의 요청 수신
 var server = app.listen(port, function () {
-    console.log("Listening on port %d", server.address().port);
+  콘솔.log("포트 % d에서 수신 중", 서버.주소().포트);
 });
